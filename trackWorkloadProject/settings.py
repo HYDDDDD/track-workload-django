b41b6487d43b65cd63a1df87f071654cb1eac847
Kantamas <kantamas35248@gmail.com>
2024-02-14 14:15:29 +0700

    Update deploy

--- trackWorkloadProject/settings.py
+++ trackWorkloadProject/settings.py
@@ -32,8 +32,11 @@ SECRET_KEY = getenv('DJANGO_SECRET_KEY', get_random_secret_key())
 
 DEBUG = getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = getenv('DJANGO_ALLOWED_HOSTS',
-                       '127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = [getenv('DJANGO_ALLOWED_HOSTS',
+                        '127.0.0.1,localhost').split(','), '.vercel.app']
+
+# ALLOWED_HOSTS = getenv('DJANGO_ALLOWED_HOSTS',
+#                        '127.0.0.1,localhost').split(',')
 
 CORS_ALLOW_HEADERS = [
     'access-control-allow-origin',
